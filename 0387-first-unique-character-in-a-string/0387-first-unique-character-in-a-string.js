8266ed2d38fa77f7d52986f648bc61e1630f63a6
ABIN JILS <abinjills0904@gmail.com>
2025-11-21 19:43:35 +0530

    Time: 33 ms (55.52%), Space: 59.6 MB (45.67%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+    const count = {};
+
+    // Count each character
+    for (let char of s) {
+        count[char] = (count[char] || 0) + 1;
+    }
+
+    // Find first character with count 1
+    for (let i = 0; i < s.length; i++) {
+        if (count[s[i]] === 1) {
+            return i;
+        }
+    }
+
+    return -1; 
+};
